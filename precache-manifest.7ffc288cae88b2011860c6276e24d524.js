441ccfb916b6223cb53f32dfd667b53abd8576fa
Carl-Erik Kopseng <carlerik@gmail.com>
2025-10-26 20:21:39 +0100

    deploy with CF workers

---
+++ precache-manifest.7ffc288cae88b2011860c6276e24d524.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04799ad2e5a45830c0b8e73774f7a165",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d9d8a08d82b5ca78e87",
+    "url": "/static/css/main.3cd1dfc5.chunk.css"
+  },
+  {
+    "revision": "4abf3e1330fd0c87af2f",
+    "url": "/static/js/2.92c5db8b.chunk.js"
+  },
+  {
+    "revision": "2d9d8a08d82b5ca78e87",
+    "url": "/static/js/main.6e94a8ab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
